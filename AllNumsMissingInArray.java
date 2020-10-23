bd74e81d365e8dd00bb23612c76004d16e52ab02
lakshmi2812 <lakshmi.nishthala@gmail.com>
2020-10-22 22:30:03 -0700

    Accepted solution for AllNumbersMissingInArray problem

--- AllNumsMissingInArray.java
+++ AllNumsMissingInArray.java
@@ -1,39 +1,39 @@
 import java.util.HashMap;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.List;
 
+//Problem -> Find all numbers disappeared in an array
 class AllNumsMissingInArray{
 
-    public static int[] allNumsMissingInArray(int[] arr){
+    public static List<Integer> allNumsMissingInArray(int[] nums){
         ArrayList<Integer> missing_nums = new ArrayList<Integer>();
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
         //put integers from 1 to array size(n) as keys in the hashMap, each with value 0
-        for(int i  = 1; i <= arr.length; i++){
+        for(int i  = 1; i <= nums.length; i++){
             hashMap.put(i,0);
         }
         //update hashMap
-        for(int i = 0; i < arr.length; i++){
-            int current = arr[i];
+        for(int i = 0; i < nums.length; i++){
+            int current = nums[i];
             int value = hashMap.get(current);
             hashMap.replace(current, value, value+1);
         }
 
-        //get all keys in the hashMap
-        Set<Integer> keys = hashMap.keySet();
-        Integer[] keys_Integer = keys.toArray(new Integer[keys.size()]);
-
-        for(int i = 0; i < keys_Integer.length; i++){
-            if(hashMap.get(keys_Integer) == 0){
-                int missing_int = keys_Integer[i].intValue();
-                missing_nums.add(missing_int);
+        //iterate through the hashMap
+        hashMap.forEach((k,v)->{
+            if(v == 0){
+                missing_nums.add(k);
             }
-        }
-
-        return missing_nums_array;
+        });
+        return missing_nums;
     }
     public static void main(String[] args){
         int[] arr = {4,3,2,7,8,2,3,1};
-        int[] missing_nums_array = allNumsMissingInArray(arr);
+        List<Integer> missing_nums = allNumsMissingInArray(arr);
+        for(int i = 0; i < missing_nums.size(); i++){
+            System.out.println(missing_nums.get(i));
+        }
     }
 
 }
